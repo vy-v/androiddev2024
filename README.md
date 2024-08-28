15dc35e60729bb610c5cdba7f0b0ed62d628cd3c
Ha Vy Vu <havyvu@Vys-MacBook-Pro.local>
2024-08-28 15:58:18 +0700

    Prac 3

---
+++ README.md
@@ -0,0 +1,16 @@
+USTH ICT 2024 Android Application Development
+=====================================================
+
+Students are expected to:
+
+* Fork this repository to your github account
+* Update student name and ID to this README file
+* Push your commits regularly, with proper commit messages
+
+Student Info
+=======================
+
+* Name: Vũ Hà Vy
+* ID: 22bi13485
+* Group ID: *Your Group ID here*
+* Project Name: *Your Project Name here*
---
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -0,0 +1,64 @@
+package vn.edu.usth.usthweather;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    public static String TAG = "WeatherActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ForecastFragment firstFragment = new ForecastFragment();
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(R.id.main, firstFragment).commit();
+    }
+
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        Log.i(TAG, "on pause");
+    }
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        Log.i(TAG, "on start");
+    }
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Log.i(TAG, "on resume");
+    }
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        Log.i(TAG, "on stop");
+    }
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.i(TAG, "on destroy");
+    }
+
+}
