f4f045dfc28eb8ccc287668a002fefaafba3e2e5
vy-v <vyhavu.hn@gmail.com>
2024-10-01 22:21:05 +0700

    redo prac11

--- USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -11,13 +11,26 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager2.widget.ViewPager2;
-import android.media.MediaPlayer;
 
+import android.media.MediaPlayer;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.os.Build;
+import android.widget.Toast;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class WeatherActivity extends AppCompatActivity {
 
     public static String TAG = "WeatherActivity";
+    private static final int REQUEST_WRITE_STORAGE = 112;
+    private static final String MP3_FILE_NAME = "your_audio_file.mp3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,21 +51,84 @@ public class WeatherActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(pager);
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(WeatherActivity.this, R.raw.sample_music);
-        mediaPlayer.setLooping(true);
-        mediaPlayer.start();
+        requestStoragePermissions();
 
+        copyFileToExternalStorage(R.raw.music, "music.mp3");
 
+        playMusic();
 
-
-        //WeatherFragment firstFragment = new WeatherFragment();
-        //getSupportFragmentManager().beginTransaction().add(R.id.main, firstFragment).commit();
-
-        //ForecastFragment secondFragment = new ForecastFragment();
-        // Add the fragment to the 'container' FrameLayout
-        // getSupportFragmentManager().beginTransaction().add(R.id.main, secondFragment).commit();
     }
 
+    private void requestStoragePermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
+            } else {
+                // Permission denied
+            }
+        }
+    }
+
+
+
+    private void copyFileToExternalStorage(int resourceId, String fileName) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            // Input stream to read from the resource
+            in = getResources().openRawResource(resourceId);
+
+            // Path where you want to copy the file
+            File path = Environment.getExternalStorageDirectory();
+            File file = new File(path, fileName);
+
+            // Output stream to write to the destination
+            out = new FileOutputStream(file);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+
+            // Close streams
+            in.close();
+            out.close();
+
+            Toast.makeText(this, "File copied to external storage", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void playMusic() {
+        File path = Environment.getExternalStorageDirectory();
+        File file = new File(path, "music.mp3");
+
+        if (file.exists()) {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            try {
+                mediaPlayer.setDataSource(file.getPath());
+                mediaPlayer.prepare();
+                mediaPlayer.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(this, "File not found", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     public void onPause()
@@ -60,24 +136,28 @@ public class WeatherActivity extends AppCompatActivity {
         super.onPause();
         Log.i(TAG, "on pause");
     }
+
     @Override
     public void onStart()
     {
         super.onStart();
         Log.i(TAG, "on start");
     }
+
     @Override
     public void onResume()
     {
         super.onResume();
         Log.i(TAG, "on resume");
     }
+
     @Override
     public void onStop()
     {
         super.onStop();
         Log.i(TAG, "on stop");
     }
+
     @Override
     public void onDestroy()
     {
